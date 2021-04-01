645a2477f2aa5afa6ddd582bf2e3c912e0912b93
Korneel Eeckhout <korneel@pienter.tech>
2021-04-01 11:04:10 +0200

    quizr - fetch

--- 05-quizr/script.js
+++ 05-quizr/script.js
@@ -1,55 +1,11 @@
-const questions = [
-    {
-        question: 'Linus Torvalds created Linux and Git.',
-        correctAnswer: 'True',
-    },
-    {
-        question:
-            'The programming language "Python" is based off a modified version of "JavaScript".',
-        correctAnswer: 'False',
-    },
-    {
-        question: 'The logo for Snapchat is a Bell.',
-        correctAnswer: 'False',
-    },
-    {
-        question: 'RAM stands for Random Access Memory.',
-        correctAnswer: 'True',
-    },
-    {
-        question: '"HTML" stands for Hypertext Markup Language.',
-        correctAnswer: 'True',
-    },
-    {
-        question:
-            'In most programming languages, the operator ++ is equivalent to the statement "+= 1".',
-        correctAnswer: 'True',
-    },
-    {
-        question: 'The Windows 7 operating system has six main editions.',
-        correctAnswer: 'True',
-    },
-    {
-        question:
-            'The Windows ME operating system was released in the year 2000.',
-        correctAnswer: 'True',
-    },
-    {
-        question:
-            'The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.',
-        correctAnswer: 'False',
-    },
-    {
-        question: 'Linux was first created as an alternative to Windows XP.',
-        correctAnswer: 'False',
-    },
-];
-
 const $score = document.getElementById('total-correct');
 const $question = document.getElementById('question');
+const $questionContainer = document.getElementById('question-container');
 const $trueBtn = document.getElementById('answer-true');
 const $falseBtn = document.getElementById('answer-false');
 const $resultContainer = document.getElementById('result-container');
+const $categorySelector = document.getElementById('category-selector');
+const $difficultySelector = document.getElementById('difficulty-selector');
 
 const state = {
     questions: [],
@@ -87,23 +43,62 @@ function printQuestion() {
             'Your score is ' + state.score + '. Want to try again?';
         return;
     }
-    $question.textContent = state.questions[state.index].question;
+    $question.innerHTML = state.questions[state.index].question;
+}
+
+function getUrl() {
+    const category = $categorySelector.value;
+    const difficulty = $difficultySelector.value;
+
+    return (
+        'https://opentdb.com/api.php?amount=10&type=boolean' +
+        (category === 'any' ? '' : `&category=${category}`) +
+        (difficulty === 'any' ? '' : `&difficulty=${difficulty}`)
+    );
+}
+
+function getQuestions() {
+    return fetch(getUrl())
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error('Could not fetch questions');
+            }
+
+            return response.json();
+        })
+        .then(function (body) {
+            if (body.response_code !== 0) {
+                throw new Error('Could not fetch questions');
+            }
+
+            return body.results;
+        });
 }
 
 function resetGame() {
-    state.questions = scrambleQuestions();
+    $questionContainer.classList.add('loading');
     state.index = 0;
     state.score = 0;
     resetQuestionLabels();
     printScore();
-    printQuestion();
+    getQuestions()
+        .then(function (questions) {
+            state.questions = questions;
+            printQuestion();
+            $questionContainer.classList.remove('loading');
+        })
+        .catch(function (error) {
+            console.error(error);
+            initGame('We did not find enough questions in this category.');
+            $questionContainer.classList.remove('loading');
+        });
 }
 
 function answerQuestion(answer) {
     const $resultLabel = $resultContainer.children[state.index];
     $resultLabel.classList.remove('is-dark');
 
-    if (state.questions[state.index].correctAnswer === answer) {
+    if (state.questions[state.index].correct_answer === answer) {
         $resultLabel.classList.add('is-success');
         state.score++;
     } else {
@@ -116,6 +111,11 @@ function answerQuestion(answer) {
 }
 
 function trueBtnClicked() {
+    if (state.index === -1) {
+        resetGame();
+        return;
+    }
+
     if (state.index === state.questions.length) {
         resetGame();
         return;
@@ -124,32 +124,25 @@ function trueBtnClicked() {
 }
 
 function falseBtnClicked() {
-    if (state.index === state.questions.length) {
+    if (state.index === state.questions.length || state.index === -1) {
         return;
     }
     answerQuestion('False');
 }
 
-resetGame();
+function initGame(error) {
+    state.index = -1;
+    state.score = 0;
+    resetQuestionLabels();
+    printScore();
+    $question.innerHTML =
+        (error ? error + '<br>' : '') +
+        'Choose ' +
+        (error ? 'another' : 'a') +
+        ' category and difficulty. <br> Are you ready?';
+}
+
+initGame();
 
 $trueBtn.addEventListener('click', trueBtnClicked);
 $falseBtn.addEventListener('click', falseBtnClicked);
-
-fetch('https://opentdb.com/api.php?amount=10&type=boolean')
-    .then(function (response) {
-        if (!response.ok) {
-            throw new Error('Could not fetch questions');
-        }
-
-        return response.json();
-    })
-    .then(function (body) {
-        if (body.response_code !== 0) {
-            throw new Error('Could not fetch questions');
-        }
-
-        console.log(body.results);
-    })
-    .catch(function (error) {
-        console.error(error);
-    });
